a8e61b30aa4b8b08b3abc2f7c20aa5458276def8
mcfreis <mcfreis@gmx.net>
2018-04-05 17:12:40 +0200

    Added Size1 and Size2 options; updated unittests; multicast under Windows working

--- coverage_test_multicast_ipv6.py
+++ coverage_test_multicast_ipv6.py
@@ -20,7 +20,7 @@ class Tests(unittest.TestCase):
         self.server_address = (defines.ALL_COAP_NODES_IPV6, 5683)
         self.current_mid = random.randint(1, 1000)
         self.server_mid = random.randint(1000, 2000)
-        self.server = CoAPServer("::1", 5683, multicast=True)
+        self.server = CoAPServer("::", 5683, multicast=True)
         self.server_thread = threading.Thread(target=self.server.listen, args=(10,))
         self.server_thread.start()
         self.queue = Queue()
